041cf98c4c7479d152a3f2b9df0f81cda60ef812
kstandvoss <kstandvoss@uos.de>
2017-05-22 11:53:11 +0200

    changes main

--- main.py
+++ main.py
@@ -24,55 +24,43 @@ def main(args):
     val_split = 0.2
     N_CLASSES = 17
     N_SAMPLES = 40478
-
-    labels = ['blow_down',
-     'bare_ground',
-     'conventional_mine',
-     'blooming',
-     'cultivation',
-     'artisinal_mine',
-     'haze',
-     'primary',
-     'slash_burn',
-     'habitation',
-     'clear',
-     'road',
-     'selective_logging',
-     'partly_cloudy',
-     'agriculture',
-     'water',
-     'cloudy']
+    labels = list(data.LABELS.keys())
+    cross_val = False
 
     classifier = SimpleNet((size,size,3), n_classes=N_CLASSES, nb_epoch = nb_epoch, batch_size=batch_size, optimizer=optimizer)
 
     splitter = Validation_splitter('input/train_v2.csv', val_split)
-    splitter.next_fold()
-
-    reader = CSV_line_reader('input/train_v2.csv')
-    tg = data.train_generator('input/train-tif-v2', reader, splitter, batch_size, img_size=size, load_rgb=True)
-    vg = data.val_generator('input/train-tif-v2', reader, splitter, batch_size, img_size=size, load_rgb=True)
-
-    print('start training: ')
-    classifier.fit(tg, vg, ((1-val_split) * N_SAMPLES, val_split * N_SAMPLES))
-
-    
-    print('validating')
-    val_data, val_labels = data.get_all_val('input/train-tif-v2', reader, splitter, img_size=size, load_rgb=True)
-    pdb.set_trace()
-    p_valid = classifier.predict(val_data)
-
-    print('validation loss: {}'.format(fbeta_score(val_labels, np.array(p_valid) > 0.2, beta=2, average='samples')))
-
-    print('save model:')
-    classifier.model.save(os.path.join('models', 'simple_net_{}'.format(loss)))
-
-    thres_opt = utils.optimise_f2_thresholds(val_labels, p_valid) 
-
     test_data = data.get_all_test('input/test-tif-v2', img_size=size, load_rgb=True)
+    val_data, val_labels = data.get_all_val('input/train-tif-v2', reader, splitter, img_size=size, load_rgb=True)
+
+    result = np.zeros((len(test_data),N_CLASSES))
+    while(splitter.next_fold() and cross_val):
+
+        reader = CSV_line_reader('input/train_v2.csv')
+        tg = data.train_generator('input/train-tif-v2', reader, splitter, batch_size, img_size=size, load_rgb=True)
+        vg = data.val_generator('input/train-tif-v2', reader, splitter, batch_size, img_size=size, load_rgb=True)
+
+        print('start training: ')
+        classifier.fit(tg, vg, ((1-val_split) * N_SAMPLES, val_split * N_SAMPLES))
+
+        
+        print('validating')
+        #pdb.set_trace()
+        p_valid = classifier.predict(val_data)
+
+        print('validation loss: {}'.format(fbeta_score(val_labels, np.array(p_valid) > 0.2, beta=2, average='samples')))
+
+        print('save model:')
+        classifier.model.save(os.path.join('models', 'simple_net_{}'.format(loss)))
+
+        thres_opt = utils.optimise_f2_thresholds(val_labels, p_valid) 
+        
+        p_test = classifier.predict(test_data)
+        result += p_test
     
-    p_test = classifier.predict(test_data)
+    result /= splitter.num_folds
+    result = pd.DataFrame(result, columns = labels)    
     
-    result = pd.DataFrame(p_test, columns = labels)
 
     preds = []
     for i in tqdm(range(result.shape[0]), miniters=1000):
